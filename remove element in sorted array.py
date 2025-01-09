3e766c060246a5451d439a39e8780e0ca9d53619
Premraaj <116739941+premraaj002@users.noreply.github.com>
2025-01-09 19:34:02 +0530

    Update remove element in sorted array.py

--- remove element in sorted array.py
+++ remove element in sorted array.py
@@ -1,9 +1,11 @@
+# LEETCODE PROBLEM NO 26 = REMOVE DUPLICATED IN SORTED ARRAY
+
 arr=[0,0,1,1,1,2,2,3,3,4]
-arr1=[]
+arr1=set() # or arr1=[]
 k=0
 for i in range(len(arr)):
     if arr[i] not in arr1:
-        arr1.append(arr[i])
+        arr1.add(arr[i]) # or arr1.append(arr[i])
         arr[k]=arr[i]
         k+=1
 print(k)
